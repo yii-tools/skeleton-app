f9d2701b4144523dd7e527106cc135086cabd37a
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-06-16 05:50:47 -0400

    Add `LocaleRouteHelper::class`. (#8)

--- config/web/param/yiisoft-view.php
+++ config/web/param/yiisoft-view.php
@@ -6,10 +6,10 @@ use Yii\Service\ParameterInterface;
 use Yiisoft\Assets\AssetManager;
 use Yiisoft\Csrf\CsrfTokenInterface;
 use Yiisoft\Definitions\Reference;
-use Yiisoft\Router\CurrentRoute;
 use Yiisoft\Router\UrlGeneratorInterface;
 use Yiisoft\Session\Flash\FlashInterface;
 use Yiisoft\Translator\TranslatorInterface;
+use Yii\Middleware\LocaleRouteHelper;
 
 return [
     'yiisoft/view' => [
@@ -17,7 +17,7 @@ return [
         'parameters' => [
             'assetManager' => Reference::to(AssetManager::class),
             'csrfToken' => Reference::to(CsrfTokenInterface::class),
-            'currentRoute' => Reference::to(CurrentRoute::class),
+            'localeRouteHelper' => Reference::to(LocaleRouteHelper::class),
             'flash' => Reference::to(FlashInterface::class),
             'parameter' => Reference::to(ParameterInterface::class),
             'translator' => Reference::to(TranslatorInterface::class),
