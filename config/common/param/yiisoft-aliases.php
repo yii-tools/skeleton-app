fced88851739cc35f1793bb90d754e43368a4e43
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-06-17 11:31:02 -0400

    Remove unnecesary translator. (#10)

--- config/common/param/yiisoft-aliases.php
+++ config/common/param/yiisoft-aliases.php
@@ -5,17 +5,15 @@ declare(strict_types=1);
 return [
     'yiisoft/aliases' => [
         'aliases' => [
-            '@app' => dirname(__DIR__, 2),
+            '@root' => dirname(__DIR__, 2),
             '@assets' => '@public/assets',
             '@assetsUrl' => '@baseUrl/assets',
             '@baseUrl' => '/',
-            '@messages' => '@resources/messages',
             '@npm' => '@root/node_modules',
             '@public' => '@root/public',
             '@resources' => '@root/src/Framework/resources',
             '@runtime' => '@root/runtime',
             '@vendor' => '@root/vendor',
-            '@views' => '@resources/views',
         ],
     ],
 ];
