1001a4217bccf2cfb5f4a3fe94daf30c2be815f6
Wilmer Arambula <terabytesoftw@gmail.com>
2023-06-19 08:32:47 -0400

    Add spanish locale.

--- config/common/param/yii-tools-middleware.php
+++ config/common/param/yii-tools-middleware.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'yii-tools/middleware' => [
         'locale' => [
-            'languages' => ['en' => 'en-US', 'ru' => 'ru-RU'],
+            'languages' => ['en' => 'en-US', 'ru' => 'ru-RU', 'es' => 'es-ES'],
             'ignoredUrls' => ['/debug**'],
         ],
     ],
