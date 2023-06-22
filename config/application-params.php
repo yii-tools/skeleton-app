33f384ac4fda203cf10adb7408364501c50ef124
Wilmer Arambula <terabytesoftw@gmail.com>
2023-06-22 08:11:55 -0400

    Fix `application-params.php`.

--- config/application-params.php
+++ config/application-params.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
-// Parameter for the application.
+// parameter for the application.
 
 return [
     'app' => [
         'charset' => 'UTF-8',
-        'name' => 'My Project',
-        'description' => 'Welcome to My Project',
+        'name' => 'Yii App',
     ],
 ];
